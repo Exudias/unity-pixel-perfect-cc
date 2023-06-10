b35422f301937b9dd5a0df8f18a669965ab56838
Exudias <editoo31@gmail.com>
2023-06-10 12:30:41 +0300

    Add documentation to PPSettings, make it static too

--- Assets/Scripts/PPSettings.cs
+++ Assets/Scripts/PPSettings.cs
@@ -1,6 +1,10 @@
-using UnityEngine;
-
-public class PPSettings
+/// <summary>
+/// Settings for the pixel-perfect game.
+/// </summary>
+public static class PPSettings
 {
+    /// <summary>
+    /// The amount of pixels in one Unity unit.
+    /// </summary>
     public static int PIXELS_PER_UNIT = 8;
 }
